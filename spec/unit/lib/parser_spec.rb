9d453aeb360b6bdee01e6bbb8e62513f9b170435
AG-Sile <annaglusz@gmail.com>
2019-09-17 16:59:55 -0500

    more tests

--- spec/unit/lib/parser_spec.rb
+++ spec/unit/lib/parser_spec.rb
@@ -59,6 +59,37 @@ describe Parser do
       }])
 
     end
+    it "is happy with multiple pages" do
+      allow(subject).to receive(:open).and_return(:html_instance)
+      allow(Nokogiri::HTML::Document).to receive(:parse).with(:html_instance, nil, nil, 2145).and_return(noko_instance)
+      noko_instance.stub_chain(:css, :children, :text).and_return("101 Reviews")
+      noko_instance.stub_chain(:css, :at, :get_attribute).and_return("12345")
+
+      stub_request(:get, request_to_be_stubbed)
+        .with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Host'=>'www.lendingtree.com',
+          'User-Agent'=>'Ruby'
+           }
+        )
+        .to_return(status: 200, body: fake_good_response.to_json)
+      subject.parse_reviews("www.example.com").should eq([{
+        :title=>"The REVIEW", 
+        :review_text=>"Just look at how awesome my review is!!", 
+        :author_name=>"Mr Kittens", 
+        :author_email=>"kittens@example.com", 
+        :author_location=>"Dark Alley, IL", 
+        :is_recommended=>"true", 
+        :review_likes=>"9001", 
+        :review_dislikes=>"0", 
+        :overall_rating=>"5", 
+        :secondary_rating=>{}, 
+        :review_dateTime=>dateTimeNow.to_s
+      }])
+
+    end
   end
 
   describe "the api returns an error" do 
