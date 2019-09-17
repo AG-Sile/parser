fba5801b96d1bba46802ed3805771ae5af61b67d
AG-Sile <annaglusz@gmail.com>
2019-09-17 13:43:52 -0500

    just casually change the entire approach

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,5 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
+require 'net/http'
+require 'uri'
+require 'json'
 
 class Parser
 
@@ -9,33 +12,44 @@ class Parser
     site = Nokogiri::HTML(open(url))
     # grab total review count
     reviews_count = site.css('a[class=reviews-count]').children.text.split(" ").first.to_i
-    pages = reviews_count
-    review_pages_threads = (1..(reviews_count/10.0).ceil).map do |pg_n|
-      Thread.new do 
-        new_url = url + "?sort=b3ZlcmFsbHJhdGluZ19kZXNj&pid=#{pg_n}"
-        Nokogiri::HTML(open(new_url)).css('div.col-xs-12.mainReviews')
-      end
-    end
-    review_infos = review_pages_threads.map do |t|
-      tvalue = t.value
-      tvalue.map { |review| get_review_info(review) }
-    end.flatten
-    review_infos
+    brand_id = site.css('a.reviewBtn.write-review').at('a').get_attribute('data-lenderreviewid')
+    return get_review_threads(reviews_count, brand_id).map(&:value).flatten
   end
 
   private
 
   def get_review_info(review)
       {
-        :stars => review.css('div.numRec').text[/[\d]/].to_i,
-        :title => review.css('p.reviewTitle').text,
-        :review_text => review.css('p.reviewText').text,
-        :reviewer_name => review.css('p.consumerName').children.first.text.strip,
-        :reviewer_location => review.css('p.consumerName').children.last.text[5..-1].strip,
-        :review_date => review.css('p.consumerReviewDate').text[12..-1].strip,
-        :review_likes => review.css('span.likes').text.to_i,
-        :review_dislikes => review.css('span.dislikes').text.to_i
+        :title => review['title'],
+        :review_text => review['text'],
+        :author_name => review['authorName'],
+        :author_email => review['authorEmail'],
+        :author_location => review['userLocation'],
+        :is_recommended => review['isRecommended'],
+        :review_likes => review['votesUp'],
+        :review_dislikes => review['votesDown'],
+        :overall_rating => review['primaryRating'],
+        :secondary_rating => review['secondaryRatings'],
+        :review_dateTime => review['submissionDateTime'] 
       }
   end 
-end
 
+  def get_review_threads(reviews_count, lender_reviews_id)
+    base_url = "https://www.lendingtree.com/content/mu-plugins/lt-review-api/review-api-proxy.php?RequestType=&productType=&brandId=#{lender_reviews_id}&requestmode=reviews,stats,ratingconfig,propertyconfig&sortby=reviewsubmitted&sortorder=desc&pagesize=100"
+    review_threads = (0..(reviews_count/100.0).ceil).map do |pg_n|
+      Thread.new do
+        retries = 5
+        uri = base_url + "&page=#{pg_n}"
+        response = Net::HTTP.get_response(URI.parse(uri))
+        begin
+          reviews = JSON.parse(response.body)['result']['reviews']
+          reviews.map { |r| get_review_info(r) }
+        rescue NoMethodError => e
+          e if retry_count <= 0
+          retry_count -= 1
+          retry
+        end
+      end
+    end
+  end
+end
